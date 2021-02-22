ef2bf67273ab4f2bfa880c8d7f575969bb9a7aa9
raccoon <raccooncoon@kakao.com>
2021-02-22 15:40:29 +0900

    add drawer and router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import About from "./About";
 import "./App.css";
+import Contact from "./Contact";
+import Drawer from "./Drawer";
 import Home from "./Home";
 
 const useStyles = makeStyles({
@@ -14,10 +17,15 @@ export default function App() {
   const classes = useStyles();
   return (
     <div className={classes.container}>
+      <Drawer />
       <Switch>
         <Route exact from="/" render={(props) => <Home {...props} />} />
-        {/*<Route exact path="/contact" render={props => <Contact {...props} />} />*/}
-        {/*<Route exact path="/about" render={props => <About {...props} />} />*/}
+        <Route
+          exact
+          path="/contact"
+          render={(props) => <Contact {...props} />}
+        />
+        <Route exact path="/about" render={(props) => <About {...props} />} />
       </Switch>
     </div>
   );
